3728debf941aa655b31dcaa50bd6790ac46abc21
gabrielegrillo <gabriele.grillo.2003@gmail.com>
2025-01-31 11:30:15 +0100

    exercise model done

--- src/main/java/it/unical/web/backend/Persistence/Model/Exercise.java
+++ src/main/java/it/unical/web/backend/Persistence/Model/Exercise.java
@@ -1,4 +1,21 @@
 package it.unical.web.backend.Persistence.Model;
+import lombok.*;
+import java.util.Date;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Exercise {
+    private long id;
+    private User user;
+    private Date date;
+    private int sets;
+    private int reps;
+    private int weightUsed;
+    private int timePassed;
+    private String name;
+    private String targetMuscleGroup;
+    private int kcaloriesBurnedPerRep;
 }

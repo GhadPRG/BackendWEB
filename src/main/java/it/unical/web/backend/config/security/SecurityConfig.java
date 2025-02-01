b6d60fbf25547efd67d3ec8a8fe560e988a1c495
Gabriele Grillo <gabriele.grillo.2003@gmail.com>
2025-02-01 10:28:36 +0100

    started on JWT implementation

---
+++ src/main/java/it/unical/web/backend/config/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package it.unical.web.backend.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login", "/api/register").permitAll()
+                        .requestMatchers("/api/auth/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .formLogin(form -> form
+                        .loginProcessingUrl("/api/login")
+                        .successHandler((req, res, auth) -> res.setStatus(200))
+                        .failureHandler((req, res, ex) -> res.setStatus(401))
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/logout")
+                        .logoutSuccessHandler((req, res, auth) -> res.setStatus(200))
+                );
+        return http.build();
+    }
+}
---
+++ src/main/java/it/unical/web/backend/service/JWTService.java
@@ -0,0 +1,64 @@
+package it.unical.web.backend.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+import static io.jsonwebtoken.SignatureAlgorithm.HS256;
+import java.security.Key;
+import java.util.Date;
+
+
+@Service
+public class JWTService {
+
+    private final String SECRET_KEY;
+
+    public JWTService(@Value(staticConstructor = "${jwt.secret}") String SECRET_KEY) {
+        this.SECRET_KEY = SECRET_KEY;
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String username) {
+        int expiration = 1000 * 60 * 60 * 24;
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public Date extractExpirationDate(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpirationDate(token).before(new Date());
+    }
+
+    public boolean validateToken(String token, String username) {
+        System.out.println("Username extracted: " + extractUsername(token));
+        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
+    }
+}

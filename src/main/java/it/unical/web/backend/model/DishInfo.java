495c48b625e7670d21ddafe7069139f437b32d66
Giovanni Muraca <ghadprogrammer@gmail.com>
2025-02-01 20:35:58 +0100

    Aggiunti nuovi model per poter gestire in modo più ordinato MealDAO

---
+++ src/main/java/it/unical/web/backend/model/DishInfo.java
@@ -0,0 +1,31 @@
+package it.unical.web.backend.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class DishInfo {
+    private long id;
+    private String nome;
+    private int kcal;
+    private int carbs;
+    private int proteins;
+    private int fats;
+    private int fibers;
+    private Long user_id;
+
+    public DishInfo(String nome, int kcal, int carbs, int proteins, int fats, int fibers, Long user_id) {
+        this.nome = nome;
+        this.kcal = kcal;
+        this.carbs = carbs;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.fibers = fibers;
+        this.user_id = user_id;
+    }
+}

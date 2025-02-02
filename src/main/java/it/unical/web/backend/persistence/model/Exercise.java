bac1145f43f396442be099569960e8d44d5159f5
Giovanni Muraca <ghadprogrammer@gmail.com>
2025-02-02 18:55:41 +0100

    Modificato model exercise per il motivo descritto prima (ho dimenticato di aggiungere questo file insieme agli altri inerenti agli esercizi)

--- src/main/java/it/unical/web/backend/persistence/model/Exercise.java
+++ src/main/java/it/unical/web/backend/persistence/model/Exercise.java
@@ -1,21 +1,34 @@
 package it.unical.web.backend.persistence.model;
 import lombok.*;
+
+import javax.management.ConstructorParameters;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Exercise {
-    private long id;
-    private User user;
-    private Date date;
-    private int sets;
-    private int reps;
-    private int weightUsed;
-    private int timePassed;
+    private Long id;
     private String name;
-    private String targetMuscleGroup;
-    private int kcaloriesBurnedPerRep;
+    private String notes;
+    private String muscleGroup;
+    private int reps;
+    private int sets;
+    private double kcalPerRep;
+    private Double weight; // Nullable
+    private Long createdBy;
+
+    public Exercise(String name, String notes, String muscleGroup, int reps, int sets, double kcalPerRep, Double weight, Long createdBy) {
+        this.name = name;
+        this.notes = notes;
+        this.muscleGroup = muscleGroup;
+        this.reps = reps;
+        this.sets = sets;
+        this.kcalPerRep = kcalPerRep;
+        this.weight = weight;
+        this.createdBy = createdBy;
+    }
 }
